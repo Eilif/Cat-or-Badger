3141d236f3d309b3ee30b811bba6ff3b43af62b7
Elizabeth Oakes <elizabeth.a.oakes@gmail.com>
2011-03-23 14:32:09 -0700

    changed views and stylesheet for a more coherent looking site.

--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -2,15 +2,22 @@ require 'spec_helper'
 
 describe Animal do
 
-  it 'can have a name' do
+  it 'can have a name and an image' do
     a = Animal.create!(:name => "tufted leopard", :image => File.new(Rails.root + 'spec/fixtures/images/seagull.jpg'))
     a.name.should eq("tufted leopard")
+    a.image.original_filename.should eq('seagull.jpg')
   end
 
   it 'must have a name' do
     a = Animal.new(:name => "", :image => File.new(Rails.root + 'spec/fixtures/images/seagull.jpg'))
     a.should_not be_valid
     a.should have(1).error_on(:name)
+  end
+
+  it 'must have an image' do
+    a = Animal.new(:name => "Hildegard")
+    a.should_not be_valid
+    a.should have(1).error_on(:image_file_name)
   end
 
 end
