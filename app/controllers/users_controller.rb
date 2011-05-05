b44b091e386e6e8b5ae55fe57542a7aecf6221c5
Elizabeth Oakes <elizabeth.a.oakes@gmail.com>
2011-05-05 15:45:11 -0700

    Created views for users. Tweaked the destroy method in the users controller so that it redirects to users index upon user deletion.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to root_path
+    redirect_to users_path
   end
 
 end
